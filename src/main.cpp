73d864378355b94bf27b01af6bffd8d95c140f0d
mauricio <mau.altgr@hotmail.com>
2026-02-28 12:32:45 -0300

    add first nn controlled cart pole

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,20 @@
 #include "config.h"
+#include "neat.h"
 #include "cart_pole.h"
 #include "random.h"
 #include "events.h"
 #include <SFML/Graphics.hpp>
 #include <iostream>
+
+
+std::vector<float> get_nn_input(CartPole& cart_pole){
+    std::vector<float> input_array(config::n_in, 0);
+    input_array[0] = cart_pole.x;
+    input_array[1] = cart_pole.angle;
+    input_array[2] = cart_pole.vx;
+    input_array[3] = cart_pole.w;
+    return input_array;
+}
 
 
 void print_state(CartPole& cart_pole){
@@ -21,12 +32,20 @@ int main(){
     sf::RectangleShape rectangle({ config::width, config::height });
     rectangle.setFillColor(sf::Color::White);
     rectangle.setPosition({ static_cast<float>(config::offset_x_rect), static_cast<float>(config::offset_y_rect) });
-    
+
+    std::vector<NEAT::Genome> genomes { NEAT::generate_initial_networks(config::n_pop, config::n_in, config::n_out) };
+    std::vector<float> nn_input(config::n_in, 0);
+    std::vector<float> nn_output(config::n_out, 0);
+    NEAT::Genome& genome { genomes[0] };
 
     while (window.isOpen()){
         cart_pole.force = 0;
-        process_events(window, cart_pole);
-        
+        process_events(window);
+        // process_events_user_inputs(window, cart_pole);
+        nn_input = get_nn_input(cart_pole);
+        nn_output = NEAT::forward(nn_input, genomes[0]);
+        cart_pole.force = nn_output[0] * config::max_force;
+
         dynamics(cart_pole);
         cart_pole.update();
         print_state(cart_pole);
